00274dae5bd292d7fcb0984601b961cb79504954
sharath-555 <sharathkumar6072@gmail.com>
2024-09-06 18:54:38 -0500

    Update PizzaSales.sql

--- PizzaSales.sql
+++ PizzaSales.sql
@@ -4,7 +4,7 @@
 select round(sum(total_price),2) as Total_revenue
 from pizza_sales
 
---Find out the avg order value
+--Find out the average order value
 select round(sum(total_price)/count(distinct order_id),2) as Average_order_value
 from pizza_sales
 
@@ -12,7 +12,7 @@ from pizza_sales
 select sum(quantity) as Total_pizzas_sold
 from pizza_sales
 
---findou total orders placed
+--findout total orders placed
 select count(distinct order_id) as total_orders
 from pizza_sales
 
